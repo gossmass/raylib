5e239631baa793890077e03d93f9a39f1a148daf
gossmass <multimasspain@gmail.com>
2025-08-07 00:54:51 +0900

    Added Raygui and selectable easing functions

--- interp/interp.go
+++ interp/interp.go
@@ -4,6 +4,79 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+func GetEasingFunction(easingFn Easing) EasingFunction {
+	switch easingFn {
+	default:
+		return Linear
+	case EASING_IN_QUAD:
+		return InQuad
+	case EASING_OUT_QUAD:
+		return OutQuad
+	case EASING_IN_OUT_QUAD:
+		return InOutQuad
+	case EASING_IN_CUBIC:
+		return InCubic
+	case EASING_OUT_CUBIC:
+		return OutCubic
+	case EASING_IN_OUT_CUBIC:
+		return InOutCubic
+	case EASING_IN_QUART:
+		return InQuart
+	case EASING_OUT_QUART:
+		return OutQuart
+	case EASING_IN_OUT_QUART:
+		return InOutQuart
+	case EASING_IN_QUINT:
+		return InQuint
+	case EASING_OUT_QUINT:
+		return OutQuint
+	case EASING_IN_OUT_QUINT:
+		return InOutQuint
+	case EASING_IN_SINE:
+		return InSine
+	case EASING_OUT_SINE:
+		return OutSine
+	case EASING_IN_OUT_SINE:
+		return InOutSine
+	case EASING_IN_EXPO:
+		return InExpo
+	case EASING_OUT_EXPO:
+		return OutExpo
+	case EASING_IN_OUT_EXPO:
+		return InOutExpo
+	case EASING_IN_CIRC:
+		return InCirc
+	case EASING_OUT_CIRC:
+		return OutCirc
+	case EASING_IN_OUT_CIRC:
+		return InOutCirc
+	case EASING_IN_ELASTIC:
+		return InElastic
+	case EASING_OUT_ELASTIC:
+		return OutElastic
+	case EASING_IN_OUT_ELASTIC:
+		return InOutElastic
+	case EASING_IN_BACK:
+		return InBack
+	case EASING_OUT_BACK:
+		return OutBack
+	case EASING_IN_OUT_BACK:
+		return InOutBack
+	case EASING_IN_BOUNCE:
+		return InBounce
+	case EASING_OUT_BOUNCE:
+		return OutBounce
+	case EASING_IN_OUT_BOUNCE:
+		return InOutBounce
+	case EASING_IN_SQUARE:
+		return InSquare
+	case EASING_OUT_SQUARE:
+		return OutSquare
+	case EASING_IN_OUT_SQUARE:
+		return InOutSquare
+	}
+}
+
 type Number interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"raylib/interp"
 
+	gui "github.com/gen2brain/raylib-go/raygui"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
+
+const EASING_FUNCTIONS = "Linear;InQuad;OutQuad;InOutQuad;InCubic;OutCubic;InOutCubic;InQuart;OutQuart;InOutQuart;InQuint;OutQuint;InOutQuint;InSine;OutSine;InOutSine;InExpo;OutExpo;InOutExpo;InCirc;OutCirc;InOutCirc;InElastic;OutElastic;InOutElastic;InElasticFunction;OutElasticFunction;InOutElasticFunction;InBack;OutBack;InOutBack;InBounce;OutBounce;InOutBounce;InSquare;OutSquare;InOutSquare"
 
 func getRaio() float64 {
 	screenWidth := float64(rl.GetScreenWidth())
@@ -20,8 +23,12 @@ func main() {
 	rl.SetTargetFPS(60)
 	circle_x := interp.New[int32](10)
 	circle_y := interp.New[int32](10)
+	circle_r := interp.New[float32](16.0)
+	radiusSwitch := false
 
 	camera := rl.NewCamera2D(rl.NewVector2(0, 0), rl.NewVector2(0, 0), 0, 1)
+	var dropDownActive int32 = 0
+	dropDownEditMode := false
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
@@ -35,11 +42,30 @@ func main() {
 		if rl.IsKeyPressed(rl.KeySpace) {
 			pos := rl.GetScreenToWorld2D(rl.GetMousePosition(), camera)
 
-			circle_x.SetValue(int32(pos.X)).SetDuration(1.5).SetTransition(interp.OutBack)
-			circle_y.SetValue(int32(pos.Y)).SetDuration(1.5).SetTransition(interp.OutBack)
+			circle_x.SetValue(int32(pos.X)).SetDuration(1.5)
+			circle_y.SetValue(int32(pos.Y)).SetDuration(1.5)
+
+			if radiusSwitch {
+				circle_r.SetValue(16)
+			} else {
+				circle_r.SetValue(32)
+			}
+
+			circle_r.SetDuration(1.5)
+			radiusSwitch = !radiusSwitch
 		}
-		rl.DrawCircle(circle_x.GetValue(), circle_y.GetValue(), 16.0, rl.DarkPurple)
+		rl.DrawCircle(circle_x.GetValue(), circle_y.GetValue(), circle_r.GetValue(), rl.DarkPurple)
 		rl.EndMode2D()
+
+		if gui.DropdownBox(rl.Rectangle{0, 0, 128, 24}, EASING_FUNCTIONS, &dropDownActive, dropDownEditMode) {
+			dropDownEditMode = !dropDownEditMode
+
+			fn := interp.GetEasingFunction(interp.Easing(dropDownActive))
+			circle_x.SetTransition(fn)
+			circle_y.SetTransition(fn)
+			circle_r.SetTransition(fn)
+		}
+
 		rl.EndDrawing()
 	}
 }
